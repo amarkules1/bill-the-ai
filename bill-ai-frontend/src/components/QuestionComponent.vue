62b7c03cddbd6c678416202889f62fab9322d632
Alex Markules <amarlar2354@gmail.com>
2023-10-26 21:42:22 -0400

    convert question card to its own component

---
+++ bill-ai-frontend/src/components/QuestionComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card-body">
+        <p><strong>Question:</strong> {{ q.question }}</p>
+        <p><strong>Answer:</strong> {{ q.answer }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'QuestionComponent',
+    props: {
+        q: Object
+    }
+}
+</script>
--- bill-ai-frontend/src/components/QuestionsAndAnswers.vue
+++ bill-ai-frontend/src/components/QuestionsAndAnswers.vue
@@ -29,10 +29,7 @@
     <div v-if="!loadingQuestions" class="qnaContainer">
       <div v-for="item in questionsAndAnswers" :key="item.question"
         :class="{ 'selected': item.highlighted, 'card': true, 'mb-3': true }">
-        <div class="card-body">
-          <p><strong>Question:</strong> {{ item.question }}</p>
-          <p><strong>Answer:</strong> {{ item.answer }}</p>
-        </div>
+        <QuestionComponent :q="item"/>
       </div>
     </div>
   </div>
@@ -40,49 +37,50 @@
 
 <script>
 import { useRoute } from 'vue-router';
+import QuestionComponent from './QuestionComponent.vue';
 
 export default {
-  name: 'QuestionsAndAnswers',
-
-  data() {
-    return {
-      userQuery: '',
-      queryResult: null,
-      questionsAndAnswers: [],
-      loadingQuery: false,
-      loadingQuestions: true,
-      loadingSummary: true,
-      bill_id: '',
-      billSummary: {}
-    };
-  },
-  mounted() {
-    let route = useRoute();
-    this.bill_id = route.params.bill_id;
-    this.fetchSummary()
-    this.fetchQuestionsAndAnswers();
-  },
-  methods: {
-    async fetchQuestionsAndAnswers() {
-      const response = await fetch(`/${this.bill_id}-q-and-a`);
-      this.questionsAndAnswers = await response.json();
-      this.loadingQuestions = false;
+    name: 'QuestionsAndAnswers',
+    data() {
+        return {
+            userQuery: '',
+            queryResult: null,
+            questionsAndAnswers: [],
+            loadingQuery: false,
+            loadingQuestions: true,
+            loadingSummary: true,
+            bill_id: '',
+            billSummary: {}
+        };
     },
-    async fetchSummary() {
-      const response = await fetch(`/bill-summary?bill_id=${this.bill_id}`);
-      this.billSummary = await response.json();
-      this.loadingSummary = false;
+    mounted() {
+        let route = useRoute();
+        this.bill_id = route.params.bill_id;
+        this.fetchSummary();
+        this.fetchQuestionsAndAnswers();
     },
-    async submitQuery() {
-      this.loadingQuery = true;
-      const response = await fetch(`/${this.bill_id}-query?query=${this.userQuery}`);
-      this.queryResult = await response.json();
-      this.queryResult.highlighted = true
-      this.questionsAndAnswers.unshift(this.queryResult);
-      this.userQuery = '';
-      this.loadingQuery = false;
-    }
-  }
+    methods: {
+        async fetchQuestionsAndAnswers() {
+            const response = await fetch(`/${this.bill_id}-q-and-a`);
+            this.questionsAndAnswers = await response.json();
+            this.loadingQuestions = false;
+        },
+        async fetchSummary() {
+            const response = await fetch(`/bill-summary?bill_id=${this.bill_id}`);
+            this.billSummary = await response.json();
+            this.loadingSummary = false;
+        },
+        async submitQuery() {
+            this.loadingQuery = true;
+            const response = await fetch(`/${this.bill_id}-query?query=${this.userQuery}`);
+            this.queryResult = await response.json();
+            this.queryResult.highlighted = true;
+            this.questionsAndAnswers.unshift(this.queryResult);
+            this.userQuery = '';
+            this.loadingQuery = false;
+        }
+    },
+    components: { QuestionComponent }
 };
 </script>
 
